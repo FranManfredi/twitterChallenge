a953a76f548ca6c8e144b17352bbbd73738e52f5
FranManfredi <francisco.manfredi@ing.austral.edu.ar>
2023-11-07 12:13:37 -0300

    formik implemented correctly

--- src/pages/auth/sign-in/SignInPage.tsx
+++ src/pages/auth/sign-in/SignInPage.tsx
@@ -8,17 +8,16 @@ import LabeledInput from "../../../components/labeled-input/LabeledInput";
 import Button from "../../../components/button/Button";
 import { ButtonType } from "../../../components/button/StyledButton";
 import { StyledH3 } from "../../../components/common/text";
+import { Form, Formik } from "formik";
 
 const SignInPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
   const httpRequestService = useHttpRequestService();
   const navigate = useNavigate();
   const { t } = useTranslation();
 
-  const handleSubmit = () => {
+  const handleSubmit = (email: string, password: string) => {
     httpRequestService
       .signIn({ email, password })
       .then(() => navigate("/"))
@@ -33,13 +32,27 @@ const SignInPage = () => {
             <img src={logo} alt={"Twitter Logo"} />
             <StyledH3>{t("title.login")}</StyledH3>
           </div>
+          <Formik
+            initialValues={
+              {
+                password: "",
+                email: ""
+              }}
+            onSubmit={ (values) =>{
+              handleSubmit(values.email, values.password)
+            }}
+
+                >
+          {({handleChange}) =>
+          <Form>
           <div className={"input-container"}>
             <LabeledInput
               required
               placeholder={"Enter email..."}
               title={t("input-params.email")}
               error={error}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange("email")}
+              name="email"
             />
             <LabeledInput
               type="password"
@@ -47,7 +60,8 @@ const SignInPage = () => {
               placeholder={"Enter password..."}
               title={t("input-params.password")}
               error={error}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleChange("password")}
+              name="password"
             />
             <p className={"error-message"}>{error && t("error.login")}</p>
           </div>
@@ -56,7 +70,7 @@ const SignInPage = () => {
               text={t("buttons.login")}
               buttonType={ButtonType.FOLLOW}
               size={"MEDIUM"}
-              onClick={handleSubmit}
+              type="submit"
             />
             <Button
               text={t("buttons.register")}
@@ -65,6 +79,9 @@ const SignInPage = () => {
               onClick={() => navigate("/sign-up")}
             />
           </div>
+          </Form>
+}
+          </Formik>
         </div>
       </div>
     </AuthWrapper>
