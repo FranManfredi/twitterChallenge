f05b3ec9b30b29090a01e0f5f3afd55b1fd0cdc3
FranManfredi <francisco.manfredi@ing.austral.edu.ar>
2023-11-07 12:32:19 -0300

    update some style files into styledComponents done

--- src/components/user-data-box/UserDataBox.tsx
+++ src/components/user-data-box/UserDataBox.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import Avatar from "../common/avatar/Avatar";
 import icon from "../../assets/icon.jpg";
 import { useNavigate } from "react-router-dom";
-import "./UserDataBox.css";
+import { StyledUserDataBoxUserContainer } from "./UserDataBox-user-container";
+import { StyledUserDataBoxUserInfoContainer } from "./UserInfoContainer";
+import { StyledUserDataBoxText } from "./UserDataBoxStyledText";
 
 interface UserDataBoxProps {
   name?: string;
@@ -21,7 +23,7 @@ export const UserDataBox = ({
   const navigate = useNavigate();
 
   return (
-    <div className="user-container" onClick={onClick}>
+    <StyledUserDataBoxUserContainer onClick={onClick}>
       <Avatar
         width={"48px"}
         height={"48px"}
@@ -29,11 +31,11 @@ export const UserDataBox = ({
         onClick={() => onClick ?? navigate(`/profile/${id}`)}
         alt={name ?? "Name"}
       />
-      <div className="user-info-container">
-        <p>{name ?? "Name"}</p>
-        <p style={{ color: "#566370" }}>{"@" + username ?? "@Username"}</p>
-      </div>
-    </div>
+      <StyledUserDataBoxUserInfoContainer>
+        <StyledUserDataBoxText>{name ?? "Name"}</StyledUserDataBoxText>
+        <StyledUserDataBoxText style={{ color: "#566370" }}>{"@" + username ?? "@Username"}</StyledUserDataBoxText>
+      </StyledUserDataBoxUserInfoContainer>
+    </StyledUserDataBoxUserContainer>
   );
 };
 
